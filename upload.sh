12eafca456540546b23fd555b0874c85d8d8fe22
wangchaoling3 <wangchaoling3@jd.com>
2021-03-11 11:54:28 +0800

    21-03-11 11:54:28

--- upload.sh
+++ upload.sh
@@ -4,7 +4,8 @@
 # Because MWeb can't get your github global setting.
 
 
-git add --ignore-removal .
+# git add --ignore-removal .
+git add .
 git commit -m "`date '+%y-%m-%d %H:%M:%S'`"
 git push -u origin master
 
